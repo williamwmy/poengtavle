43c2e96c985834221321805dfb80e512b1aad663
William Young <williamyoung@Williams-MacBook-Pro.local>
2025-07-03 03:18:50 +0200

    PWA/fullscreen støtte

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Poengtavle',
+        short_name: 'Poengtavle',
+        theme_color: '#17171a',
+        background_color: '#17171a',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
